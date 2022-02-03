0a973a636fd2826e2ba9fc05b127ab21d2bcca2e
MBG <81711521+bg-mohamed@users.noreply.github.com>
2022-02-03 19:23:47 +0100

    Delete CREDIT_PROJECT-main directory

--- CREDIT_PROJECT-main/simulation/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-
-admin.site.register(Pro_Situation)
-admin.site.register(Charge_Applicant)
-admin.site.register(Applicant)
--- CREDIT_PROJECT-main/simulation/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimulationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simulation'
